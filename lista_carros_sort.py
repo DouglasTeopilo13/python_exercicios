04caf1634b1b6734fc5f894e1276f16700a70bca
DouglasTeopilo13 <douglasmaiateopiloribeiro@gmail.com>
2025-06-28 23:57:08 -0400

    Essa funcao sort me complicou um pouco, derreti minha mente. amanhã eu continuo estudando

---
+++ lista_carros_sort.py
@@ -0,0 +1,19 @@
+carros = ['bmw', 'audi', 'mercedes', 'toyota', 'honda']
+carros.sort()  # Ordena a lista de carros em ordem alfabética
+print(carros)
+
+carros.sort(reverse=True) # Ordena a lista de carros em ordem alfabética reversa
+print(carros)
+
+# Exibe a lista original de carros
+carros = ['bmw', 'audi', 'mercedes', 'toyota', 'honda']
+print("Lista original de carros:")
+print(carros)
+
+print("\nLista original de carros:")
+print(sorted(carros))  # Exibe a lista original de carros em ordem alfabética
+
+print("\nLista original de carros novamente:")
+print(carros
+
+
