2a3479b0758aed91c211c2a3d935a249782e490d
DouglasTeopilo13 <douglasmaiateopiloribeiro@gmail.com>
2025-06-30 20:48:47 -0400

    Exercicios de laços

---
+++ lacos_pizza_animais.py
@@ -0,0 +1,13 @@
+pizzas = ['frango/catupiry', 'calabresa', 'atum', 'portugesa', 'lombinho']
+for sabores in pizzas:
+    print("Pizza de " + sabores.title() + " é a minha favorita" +"!\n")
+print("Gosto muito de pizza de Calabresa!" + "\n" +  "Tambem gosto pizza Portuguesa " + "\n" + "Minha preferida é Frango/catupiry!" + "\n" "Eu realmente gosto muito de pizza, rs!")
+
+
+
+animais = ['gato', 'cachorro', 'coelho']
+for caract in animais:
+    print("Um " + caract.title() + " seria um ótimo animal de estimacao" + "!\n")
+print("Todos eles são muito socialves e se dão bem com os seres humanos!")
+print("Qualquer um desses animais seria um ótimo animal de estimacão")
+    
